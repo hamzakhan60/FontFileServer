8fbe600b3a6a5d39826a735cff1ad4b75a9981d0
Hamza Khan <hghauri30@gmail.com>
2024-11-09 11:29:19 +0500

    added mockup api call

--- server.js
+++ server.js
@@ -21,6 +21,8 @@ const corsOptions = {
 // Use CORS middleware
 app.use(cors());
 
+app.use(express.json({ limit: '10mb' })); // Increase JSON payload limit to 10MB (adjust as needed)
+app.use(express.urlencoded({ limit: '10mb', extended: true })); // Increase URL-encoded payload limit
 
 
 /*step 1: upload image on supabase 
