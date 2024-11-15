cf5ee40b5a99e2b2e5a6f9456170202b7f2a3eef
Hamza Khan <124522006+hamzakhan60@users.noreply.github.com>
2024-11-16 02:50:33 +0500

    Update server.js

--- server.js
+++ server.js
@@ -27,9 +27,8 @@ const corsOptions = {
 app.use(cors());
 app.use(bodyParser.json());
 
-
-app.use(express.json({ limit: '50mb' })); // Increase JSON payload limit to 10MB (adjust as needed)
-app.use(express.urlencoded({ limit: '50mb', extended: true, parameterLimit: 10000 }));// Increase URL-encoded payload limit
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 
 
